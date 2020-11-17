17e95e733806308df914e46f17c3eeb46b9bfa9d
Gault <lucagault12@gmail.com>
2020-11-17 17:46:09 +0100

    ajout de models routes controllers mais bug

--- back/app.js
+++ back/app.js
@@ -5,13 +5,17 @@ const dotenv = require('dotenv')
 
 dotenv.config()
 
-const bdService = require('./dbServices')
-const DbServices = require('./dbServices')
+const bdService = require('./models/dbServices')
+const DbServices = require('./models/dbServices')
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+
+// app.get("/", (req, res) => {
+//     res.json({ message: "Welcome to bezkoder application." });
+// });
 
 // create
 app.post('/insert', (request, response) => {
@@ -21,24 +25,24 @@ app.post('/insert', (request, response) => {
 // read
 app.get('/getAll', (request, response) => {
     // console.log('test')
-    const db = DbServices.getDbServiceInstance()
-    const result = db.getAllData()
+    // const db = DbServices.getDbServiceInstance()
+    // const result = db.getAllData()
 
-    result
-        .then(data => response.json({
-            data: data
-        }))
-        .catch(err => console.log(err))
-        // response.json({
-        //     success: true
-        // })
+    // result
+    //     .then(data => response.json({
+    //         data: data
+    //     }))
+    //     .catch(err => console.log(err))
+    // response.json({
+    //     success: true
+    // })
 })
 
 // update
 
 // delete
 
-
+require("./routes/customer.routes.js")(app);
 
 app.listen(process.env.PORT, () => {
     console.log('app is running')
