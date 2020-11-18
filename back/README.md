ee6d4e9b30fe05e505c07953f5a18384a402c8bf
Gault <lucagault12@gmail.com>
2020-11-18 09:29:17 +0100

    add dependencies

--- back/README.md
+++ back/README.md
@@ -1,11 +1,21 @@
-Back
+in a terminal
+```	
+	$ cd back
+	$ touch .env
+	$ nano .env
+			PORT=5000 
+			USER=[user]
+			PASSWORD=[psswd]
+			DATABASE="brocoli"
+			HOST="localhost"
+			DB_PORT=3306
+	$ npm install
+	$ node app.js
+```
+in an other terminal
 
-Créer un fichier .env
-
-inclure:
-    PORT=5000
-    USER=[user]
-    PASSWORD=[password]
-    DATABASE=[db]
-    DB_PORT=3306
-    HOST=localhost
+```	
+	$ cd front
+	$ npm install
+	$ yarn run serve
+```
--- back/models/dbServices.js
+++ back/models/dbServices.js
@@ -16,33 +16,7 @@ connection.connect((error) => {
     if (error) {
         console.log(error.message)
     }
-     console.log('db ' + connection.state)
+    console.log('db ' + connection.state)
 })
 
-class DbServices {
-    static getDbServiceInstance() {
-        return instance ? instance : new DbServices()
-    }
-    async getAllData() {
-        try {
-            const response = await new Promise((resolve, reject) => {
-                const query = "SELECT * FROM test;"
-
-                connection.query(query, (err, results) => {
-                    if (err) reject(new Error(err.message))
-                    resolve(results)
-                })
-            })
-
-            console.log(response)
-            return response
-
-        } catch (error) {
-            console.log(error)
-        }
-
-    }
-}
-
-module.exports = DbServices
 module.exports = connection;
