b933860f5c2dfa3f50d24216eb4e7702f716cf7d
PietroFiani <fiani.pietro@gmail.com>
2020-11-30 21:42:01 +0100

    merge fixed

--- front/src/views/Runner/dashboard.vue
+++ front/src/views/Runner/dashboard.vue
@@ -138,7 +138,6 @@ export default {
           console.log("erreur", error);
         });
 
-<<<<<<< HEAD
       newRunner.departmentsIds.forEach((element) => {
         console.log(element)
         axios({
@@ -156,24 +155,6 @@ export default {
             });
         });
       });
-=======
-      // newRunner.departmentsIds.forEach((element) => {
-      //   axios({
-      //     method: "DELETE",
-      //     url: `http://localhost:5000/deliveries/${this.runner.id_runner}`,
-      //     headers: { "Content-Type": "application/json" },
-      //   }).then((response) => {
-      //     axios
-      //       .post("http://localhost:5000/deliveries/create", {
-      //         id_runner: this.runner.id_runner,
-      //         departmentsIds: this.runner.departmentsIds,
-      //       })
-      //       .then((response) => {
-      //         this.reload();
-      //       });
-      //   });
-      // });
->>>>>>> produit-customer
     },
   },
 };
