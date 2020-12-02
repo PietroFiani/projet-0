b9325b7780b39fa12ccbbff84d965c203b75e5ef
Tony <tony_osei@hotmail.com>
2020-12-02 19:27:32 +0100

    commande livreur marche pas

---
+++ front/src/components/Runner/VStock.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-container class="pa-4" fluid="fluid" grid-list-md="grid-list-md">
+        <v-layout wrap="wrap">
+          <v-flex xs12="xs12">
+            <h1 class="display-1 mb-1">Suggestion</h1>
+          </v-flex>
+          <v-card
+    class="mx-auto"
+    max-width="344"
+    outlined
+  >
+    <v-list-item three-line>
+      <v-list-item-content>
+        <div class="overline mb-4">
+		Article
+        </div>
+        <v-list-item-title class="headline mb-1">
+          beuh
+        </v-list-item-title>
+        <v-list-item-subtitle>de l'herbe, bien bonne</v-list-item-subtitle>
+      </v-list-item-content>
+
+      <v-list-item-avatar
+        tile
+        size="80"
+        color="grey"
+      ></v-list-item-avatar>
+    </v-list-item>
+
+    <v-card-actions>
+      <v-btn
+        outlined
+        rounded
+        text
+      >
+        +
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+        </v-layout>
+      </v-container>
+</template>
+
+<script>
+
+</script>
---
+++ front/src/components/Runner/VToolbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-toolbar>
+	<img class="logo" src='/front/src/assets/logo.svg' height="40"/>
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+    <v-toolbar-title>
+		O'Shit
+	</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+
+
+<script>
+
+</script>
+
+
+
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans&display=swap');
+
+
+</style>
