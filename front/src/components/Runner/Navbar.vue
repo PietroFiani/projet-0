1fbfdbeeb23235fd5c8da58a757268ed292d66fa
Tony <tony_osei@hotmail.com>
2020-12-05 15:12:34 +0100

    finir navbar (bug)

--- front/src/components/Runner/Navbar.vue
+++ front/src/components/Runner/Navbar.vue
@@ -7,25 +7,10 @@
                <span>'Shit</span>
            </v-toolbar-title>
            <v-spacer></v-spacer>
-           <v-menu offset-y>
-      <template v-slot:activator="{ on }">
-        <v-btn
-          text
-          v-on="on"
-        >
-          <v-icon left>expand_more</v-icon>
-            <span>Menu</span>
-        </v-btn>
-      </template>
-      <v-list flat>
-        <v-list-item v-for="link in links"  :key="link.text" router :to="link.route" active-class="border">
-          <v-list-item-title >{{link.text}}</v-list-item-title>
-        </v-list-item>
-      </v-list>
-            </v-menu>
-            <v-btn text>
+           
+            <v-btn @click="logout()" text>
                 <span>Exit</span>
-                <v-icon right>exit_to_app</v-icon>
+                <v-icon right>mdi-power</v-icon>
              </v-btn>
        </v-app-bar>
       <v-navigation-drawer  v-model="drawer" dark app class="green darken-4">
@@ -34,7 +19,8 @@
                     <v-avatar size="100">
                         <v-img id="avatar" src="../../assets/avatar.png" width="200px"></v-img>
                     </v-avatar>
-                    <p class="white--text subheading mt-1 text-center">Username</p>
+                    <p class="white--text subheading mt-1 text-center">Bonjour,</p>
+					<h1 class="white--text subheading mt-1 text-center" > {{ runner.lastname }} {{ runner.firstname }} </h1>
                </v-flex>
                <v-flex class="mt-4 mb-4">
                 <Popup />
@@ -54,6 +40,7 @@
    </nav>
 </template>
 <script>
+import axios from "axios";
 import Popup from './Popup.vue'
 export default {
    data: () => ({
@@ -68,7 +55,93 @@ export default {
     components: {
     Popup
   },
-   
+  mounted() {
+    if (!this.$store.state.runnerId) {
+      this.$router.push("/");
+    } else {
+      let id = this.$store.state.runnerId;
+      let url = `http://localhost:5000/runners/${id}`;
+      axios
+        .get(url)
+        .then((response) => {
+          if (response.data) {
+            console.log("RUNNER", response.data);
+            this.runner = response.data;
+          }
+        })
+        .catch((error) => {
+          console.log("ERREUR", error);
+        });
+      let url2 = `http://localhost:5000/products/${this.$store.state.runnerId}`;
+      axios
+        .get(url2)
+        .then((response) => {
+          if (response.data) {
+            console.log("Products", response.data);
+            this.products = response.data;
+          }
+        })
+        .catch((error) => {
+          console.log("ERREUR", error);
+        });
+    }
+  },
+    methods: {
+    logout() {
+      this.$store.commit("logoutRunner");
+      this.$router.push("/");
+    },
+    reload() {
+      let url2 = `http://localhost:5000/products/${this.$store.state.runnerId}`;
+      axios
+        .get(url2)
+        .then((response) => {
+          if (response.data) {
+            console.log("Products", response.data);
+            this.products = response.data;
+          }
+        })
+        .catch((error) => {
+          console.log("ERREUR", error);
+        });
+    },
+    update(newRunner) {
+      console.log("New Runner", newRunner);
+      let url = `http://localhost:5000/runners/${this.runner.id_runner}`;
+      axios
+        .put(url, {
+          id_runner: this.runner.id_runner,
+          mail: newRunner.mail,
+          phone: newRunner.phone,
+          password: newRunner.password,
+        })
+        .then((response) => {
+          console.log("Runner updated", response.data);
+        }) //c'est un objet
+        .catch((error) => {
+          console.log("erreur", error);
+        });
+
+      newRunner.departmentsIds.forEach((element) => {
+        let found = false;
+        this.runner.deliveries.forEach((actualElement) => {
+          if (element == actualElement.id_department) found = true;
+        });
+        if (found == false) {
+          axios({
+            method: "DELETE",
+            url: `http://localhost:5000/deliveries/${this.runner.id_runner}`,
+            headers: { "Content-Type": "application/json" },
+          });
+          axios.post("http://localhost:5000/deliveries/create", {
+            id_runner: this.runner.id_runner,
+            departmentsIds: this.runner.departmentsIds,
+          });
+        }
+      });
+      this.$router.go();
+    },
+  },
 }
 </script>
 <style scoped>
